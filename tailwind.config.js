a4627f5f57ff4a84f53c73ce6b0ccc4a586f8a02
Mohammed Shafin C <19749@gtl.loc>
2025-04-19 00:49:22 +0530

    finished

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,11 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
 
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        jakarta: ['"Plus Jakarta Sans"', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
